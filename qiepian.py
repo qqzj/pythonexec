649e28cb087603084f83077f3917b9abcc6c7529
wangchaojie <popxmz@Gmail.com>
2015-03-12 18:58:40 +0800

    字符串，list,tuple均可切片 起始 : 终点 : 步进

---
+++ qiepian.py
@@ -0,0 +1,18 @@
+classmates = ['apple', 'ibm', 'dell', 'lenovo', 'Oracle', 'Microsoft', 'NEC', 'Mitsubish']
+print [classmates[0],classmates[1]]
+print classmates[1:5]
+print classmates[-3:-1]
+
+ran = range(100)
+print ran[:10]
+print ran[-10:]
+print ran[:10:2]
+print ran[:]
+
+tu = ('apple', 'ibm', 'dell', 'lenovo', 'Oracle', 'Microsoft', 'NEC', 'Mitsubish')
+print tu
+print tu[::2]
+
+
+str = 'a quick brown fox jumps over the lazy dog'
+print str[14:17]
