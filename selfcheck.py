05fc0db4772ed56fdd5ea84b51b0b3e5c8231211
wangchaojie <popxmz@Gmail.com>
2015-03-16 14:49:44 +0800

    2015年3月16日14:49:23周一自检 效果还不错的哟

---
+++ selfcheck.py
@@ -0,0 +1,17 @@
+li = ['apple', 'orange', 'pinapple', 'watermelon']
+tu = ('apple', 'orange', 'pinapple', 'watermelon')
+di = {'china':'apple', 'english':'orange', 'malay':'pinapple', 'mexco':'watermelon'}
+se = [('apple', 'orange', 'pinapple', 'watermelon')]
+
+print li
+print tu
+print di
+print se
+
+print li[0]
+print tu[1]
+print di['malay']
+print li[3]
+
+ran = range(100)
+print ran[::2]
